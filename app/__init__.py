03af1e17aecc7590ec4477820fe408a013e5dc15
Phillip Cloud <cpcloud@gmail.com>
2015-03-11 14:48:19 -0400

    Remove ast junk

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,17 @@
 from __future__ import print_function, absolute_import, division
 
 import json
-import ast
 import traceback
 from cStringIO import StringIO
+from pprint import pformat
 
 from flask import Flask, render_template, Response, request
 
-from toolz import first, curry
+from toolz import first
 from toolz.compatibility import map, zip
 
 from multipledispatch import dispatch
-
-import pandas as pd
-
-from odo import odo
-from blaze import Data, compute
-from blaze.server import from_tree
-
-import blaze
+from blaze import Data, compute, join, by, transform
 
 
 app = Flask(__name__)
@@ -44,11 +37,6 @@ def table(table, n=10):
     return compute(getattr(db, table)).head(n)
 
 
-@app.route('/data/<table>/columns', methods=['GET'])
-def columns(table):
-    return list(getattr(db, table).fields)
-
-
 @app.route('/data/tables', methods=['GET'])
 def tables():
     fields = db.fields
@@ -60,76 +48,48 @@ def tables():
     return jsonify(result)
 
 
-@dispatch(ast.Expression)
-def to_json(node, scope=None):
-    return to_json(node.body, scope=scope or {})
+def get_command(r):
+    data = json.loads(request.data)
+
+    # why is this necessary?
+    params = data['params']
+    if not params:
+        return ''
+    return first(params)
 
 
-@dispatch(ast.Name)
-def to_json(node, scope=None):
-    name = node.id
-    if name in scope:
-        return {
-            'op': 'Symbol',
-            'args': [name, scope[name].dshape, None]
-        }
-    if hasattr(blaze, name.title()):
-        return name.title()
-    if hasattr(blaze.expr, name):
-        return name
-    raise NameError(name)
+@dispatch((dict, list, tuple))
+def pprint(x):
+    return pformat(x)
 
 
-@dispatch(ast.Num)
-def to_json(node, scope=None):
-    return node.n
+@dispatch(object)
+def pprint(x):
+    return repr(x)
 
 
-@dispatch(ast.Str)
-def to_json(node, scope=None):
-    return node.s
-
-
-@dispatch(ast.Call)
-def to_json(node, scope=None):
-    op = to_json(node.func, scope=scope)
-    args = list(map(curry(to_json, scope=scope), node.args))
-    if not args:
-        raise TypeError('method calls not allowed, '
-                        'use the functional form %s(...)' % op)
-    return {
-        'op': op,
-        'args': args
-    }
-
-
-@dispatch(ast.Attribute)
-def to_json(node, scope=None):
-    return {
-        'op': 'Field',
-        'args': [to_json(node.value, scope=scope), node.attr]
-    }
+def get_traceback():
+    sio = StringIO()
+    traceback.print_exc(file=sio)
+    return sio.getvalue()
 
 
 @app.route('/compute', methods=['POST'])
 def compute_from_json():
-    data = json.loads(request.data)
-    params = data['params']
-    if not params:
-        return ''
-    query = first(params)
-
-    try:
-        raw = ast.parse(query, mode='eval')
-        js = to_json(raw, scope={'db': db})
-        expr = from_tree(js, namespace={'db': db})
-        leaf, = expr._leaves()
-        expr = expr.head(5) if hasattr(expr, 'head') else expr
-        result = repr(Data(compute(expr, {leaf: db.data}), dshape=expr.dshape))
-    except Exception:
-        sio = StringIO()
-        traceback.print_exc(file=sio)
-        result = sio.getvalue()
+    cmd = get_command(request)
+    if not cmd:
+        result = cmd
+    else:
+        try:
+            # this might be the most insecure way to do this. ever.
+            result = pprint(eval(cmd, {
+                            'db': db,
+                            'join': join,
+                            'by': by,
+                            'transform': transform
+                            }))
+        except Exception:
+            result = get_traceback()
     return jsonify({'output': result})
 
 
